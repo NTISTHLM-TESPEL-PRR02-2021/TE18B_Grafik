d5c5dd229564514af2110a67c66d80eb8118611d
Mikael <mikael.bergstrom@ga.ntig.se>
2020-10-20 19:08:51 +0200

    Initial Commit

---
+++ Graphics/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Raylib_cs;
+
+namespace Graphics
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Raylib.InitWindow(800, 600, "Hello TE!");
+
+      Color myColor = new Color(0, 255, 128, 255);
+
+      float x = 0;
+
+      while (!Raylib.WindowShouldClose())
+      {
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+        {
+          x += .1f;
+        }
+
+        Raylib.BeginDrawing();
+
+        Raylib.ClearBackground(myColor);
+
+        Raylib.DrawRectangle((int)x, 0, 20, 20, Color.PINK);
+
+
+        Raylib.EndDrawing();
+
+      }
+
+
+    }
+  }
+}
